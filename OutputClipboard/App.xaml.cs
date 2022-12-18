651c866a6a09ecbd7c8bec27eac57e0945ed7da0
sashi0034 <satoshi3104.nishi@gmail.com>
2022-12-18 11:09:17 +0900

    プロジェクト ファイルを追加します。

---
+++ OutputClipboard/App.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OutputClipboard
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private const string appName = "OutputClipboard";
+
+        public App()
+        {
+            var identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+            var principal = new System.Security.Principal.WindowsPrincipal(identity);
+
+            if (principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator))
+            {
+                setupExplolerContextMenu();
+
+                new Views.WindowFinishSetup().Show();
+            }
+            else
+            {
+                new Views.WindowOutputClipboard().Show();
+            }
+        }
+
+        // 参考: https://dobon.net/vb/dotnet/system/explorecontextmenu.html
+        private static void setupExplolerContextMenu()
+        {
+            string commandline = "\"" + $"{GetCurrentAppDir()}\\{appName}.exe" + "\" \"%1\"";
+            const string description = "クリップボードを保存(&V)";
+
+            string path = $"Directory\\background\\shell\\{appName}";
+
+            //フォルダへの関連付けを行う
+            Microsoft.Win32.RegistryKey cmdkey =
+                Microsoft.Win32.Registry.ClassesRoot.CreateSubKey(
+                    $"{path}\\command");
+            cmdkey.SetValue("", commandline);
+            cmdkey.Close();
+
+            //説明を書き込む
+            Microsoft.Win32.RegistryKey verbkey =
+                Microsoft.Win32.Registry.ClassesRoot.CreateSubKey($"{path}");
+            verbkey.SetValue("", description);
+            verbkey.Close();
+        }
+
+        public static string? GetCurrentAppDir()
+        {
+            return System.IO.Path.GetDirectoryName(
+                System.Reflection.Assembly.GetExecutingAssembly().Location);
+        }
+    }
+}
---
+++ OutputClipboard/Views/Util.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OutputClipboard.Views
+{
+    internal static class Util
+    {
+        public static void CloseWindowDelayed(Window window)
+        {
+            window.Dispatcher.Invoke(async () =>
+            {
+                await Task.Delay(2000);
+                window.Close();
+            });
+        }
+    }
+}
